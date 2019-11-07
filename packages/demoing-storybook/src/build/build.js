b1afd58c5c3d756e856ecdec0998e79bc7985863
Thomas Allmer <d4kmor@gmail.com>
2019-11-07 02:13:09 +0100

    feat: use dev server to power storybook

---
+++ packages/demoing-storybook/src/build/build.js
@@ -0,0 +1,6 @@
+const { rollup } = require('rollup');
+const createAssets = require('../create-assets');
+
+module.exports = async function build({ storybookConfigDir }) {
+  const assets = createAssets({ storybookConfigDir });
+};
